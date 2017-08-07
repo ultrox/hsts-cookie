bc569dd2ebc45b799ec06266744e77299e694fbb
Ubuntu <ubuntu@ip-172-31-24-236.eu-central-1.compute.internal>
2017-08-07 08:11:23 +0000

    Fix for mcloud

--- hsts.py
+++ hsts.py
@@ -2,7 +2,6 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.web
 
-
 class HSTSCookie(tornado.web.RequestHandler):
     def get(self):
         self.set_header('Access-Control-Allow-Origin', '*')
@@ -23,7 +22,7 @@ class HSTSScript(tornado.web.RequestHandler):
     def get(self):
         with open('hsts.js', 'r') as f:
             script = f.read()
-            script = script.replace('[HOSTNAME]', 'bugben.com')
+            script = script.replace('[HOSTNAME]', 'o.marketing-cloud.io')
             self.write(script)
 
 application = tornado.web.Application([
@@ -34,8 +33,8 @@ application = tornado.web.Application([
 
 if __name__ == '__main__':
     https_server = tornado.httpserver.HTTPServer(application, ssl_options={
-        "certfile": "keys/bugben.com.key.crt",
-        "keyfile": "keys/bugben.com.key",
+        "certfile": "/etc/letsencrypt/live/o.marketing-cloud.io/cert.pem",
+        "keyfile": "/etc/letsencrypt/live/o.marketing-cloud.io/privkey.pem",
     })
     http_server = tornado.httpserver.HTTPServer(application)
     https_server.listen(443)
